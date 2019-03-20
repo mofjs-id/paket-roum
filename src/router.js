7e5dc183900e3ed4d49c41239b5860a999cb3335
Sukirno <sukirno89@kemenkeu.go.id>
2019-03-20 11:41:18 +0700

    Add vuetify

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
+import "./plugins/vuetify";
 import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
@@ -14,9 +16,6 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
